008d536cc129883ef7fd10fb5a6fc02330dfc8d8
ZakariaBouguera <146200319+ZakariaBouguera@users.noreply.github.com>
2024-04-23 19:19:37 +0200

    interrogerUnivTulle.sql

--- interrogerUnivTulle.sql
+++ interrogerUnivTulle.sql
@@ -1,7 +1,7 @@
 use univTulle ; 
 "1/"select * from Suivre where ine = "0000000001A";
 "2/"select * from Suivre where ine != "0000000001A";
-"3/"select * from Suivre
+"3/" select libelle, count (distinct ine) from Modules inner join Suivre on Modules.idModule= Suivre.idModule group by libelle;
 "4/"select AVG(note), numEval from Passer where numEval = "1";
 "5/"select  nom, prenom, AVG(note) from Etudiant inner join Passer on Passer.ine = Etudiant.ine where nom = "KEHRE";
 "6/"select nom, prenom, AVG(note) from Passer inner join Etudiant on Passer.ine = Etudiant.ine inner join Evaluation on Evaluation.numEval = Passer.numEval where idModule = "3" group by nom, prenom;
